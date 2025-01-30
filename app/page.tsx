45f1ddfc5001ccbbc26dfea1ad9e59ac85623702
hitomi333666 <62995291+Hitomi333666@users.noreply.github.com>
2025-01-30 06:52:56 +0000

    Initial commit

---
+++ app/page.tsx
@@ -0,0 +1,17 @@
+// app/page.tsx
+import Image from "next/image";
+import Navbar from "@/components/Navbar";
+import Hero from "@/components/Hero";
+import Features from "@/components/Features";
+import Footer from "@/components/Footer";
+
+export default function Home() {
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <Features />
+      <Footer />
+    </>
+  );
+}
---
+++ components/Hero.tsx
@@ -0,0 +1,28 @@
+import Image from "next/image";
+
+export default function Hero() {
+  return (
+    <>
+      <p>Hero</p>
+      <div
+        className="hero min-h-screen"
+        style={{
+          backgroundImage: "url(/cat.jpg)",
+        }}
+      >
+        <div className="hero-overlay bg-opacity-60"></div>
+        <div className="hero-content text-neutral-content text-center">
+          <div className="max-w-md">
+            <h1 className="mb-5 text-5xl font-bold">愛猫あかさん</h1>
+            <p className="mb-5">
+              Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
+              excepturi exercitationem quasi. In deleniti eaque aut repudiandae
+              et a id nisi.
+            </p>
+            <button className="btn btn-primary">Get Started</button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
